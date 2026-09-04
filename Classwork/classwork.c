11d46121b5916a2f9a377f9b50ff3f11448d1e75
KorbinMDavis <korbinmdavis@gmail.com>
2026-09-03 19:03:39 -0500

    Completed Homework 2

--- Classwork/classwork.c
+++ Classwork/classwork.c
@@ -3,11 +3,20 @@
 
 // Comment for main
 int main(void) {
-    function();
+    //function();
     
+    double d = 2.4;
+    int n;
+
+    d = 3.6 - 1.2;
+    d = -2.4;
+    int rem = 11 % 2;
+
+    printf("1/2 = %.2lf\n", 1.0 / 2.0);
+    printf("remainder = %i\n", rem);
     return 0;
 }
 
-void function(void) {
-    //do thing
-}
+//void function(void) {
+//    //do thing
+//}
